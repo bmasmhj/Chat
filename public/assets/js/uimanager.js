bac784bbcaeb7451d04efa1f454c9ab434086a85
bmasmhj <bmasmhj@gmail.com>
2023-02-12 21:41:29 +0545

    socket message

---
+++ public/assets/js/uimanager.js
@@ -0,0 +1,11 @@
+  $('#sidemenu').click(function(){
+      $('#sidebarmenu').toggle()
+      $('#sidebarmenu').toggleClass('mobile_nav')
+    $('#chatboxsection').toggleClass('col-md-8 col-lg-9 ');
+    
+  })
+
+   $('#close_sidenav').click(function () {
+      $('#sidebarmenu').toggle()
+      $('#sidebarmenu').toggleClass('mobile_nav')
+    })
