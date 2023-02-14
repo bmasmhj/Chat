07b8db049c1d89aa84e0dd676f08541c20e64123
bmasmhj <bmasmhj@gmail.com>
2023-02-14 18:12:48 +0545

    current time

--- src/messages/addmessage.js
+++ src/messages/addmessage.js
@@ -1,7 +1,7 @@
 const database = require('../database/connection.js').database;
 
-function addmessage(uid , message){
-    database.query(`INSERT INTO message (uid , text ) VALUES ('${uid}' ,'${message}')`, function (error, results , fields) {
+function addmessage(uid , message , now_time){
+    database.query(`INSERT INTO message (uid , text , created_at ) VALUES ('${uid}' ,'${message}' , '${now_time}')`, function (error, results , fields) {
         if (error) throw error;
         return results
     });
